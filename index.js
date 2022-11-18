ae97c57db731748d917db52d1a9b83d61a5839cd
vikaskumr <vikas.kumar@contenful.com>
2022-11-18 16:44:24 +0530

    feat: custom github action to deploy apps to contentful

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { upload } = require("@contentful/app-scripts");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
+  const organizationId = core.getInput("organization-id");
+  console.log(`OrganizationId ${organizationId}!`);
+
+  const appDefinitionId = core.getInput("app-definition-id");
+  console.log(`appDefinitionId ${appDefinitionId}!`);
+
+  const accessToken = core.getInput("access-token");
+
+  const folder = core.getInput("folder");
+  console.log(`folder ${folder}!`);
+
+  upload.nonInteractive({
+    "bundle-dir": folder,
+    "organization-id": organizationId,
+    "definition-id": appDefinitionId,
+    token: accessToken,
+  });
+
+  const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
