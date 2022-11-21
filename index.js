bb545a133468d6664bf698b11a0405b068bfcb60
vikaskumr <vikas.kumar@contenful.com>
2022-11-21 17:17:37 +0530

    fix: testing

--- index.js
+++ index.js
@@ -2,35 +2,34 @@ import core from "@actions/core";
 import github from "@actions/github";
 import { upload } from "@contentful/app-scripts";
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  deploy();
-} catch (error) {
-  core.setFailed(error.message);
-}
-
 const deploy = async function () {
-  const organizationId = core.getInput("organization-id");
-  console.log(`OrganizationId ${organizationId}!`);
+  try {
+    const organizationId = core.getInput("organization-id");
+    console.log(`OrganizationId ${organizationId}!`);
 
-  const appDefinitionId = core.getInput("app-definition-id");
-  console.log(`appDefinitionId ${appDefinitionId}!`);
+    const appDefinitionId = core.getInput("app-definition-id");
+    console.log(`appDefinitionId ${appDefinitionId}!`);
 
-  const accessToken = core.getInput("access-token");
+    const accessToken = core.getInput("access-token");
 
-  const folder = core.getInput("folder");
-  console.log(`folder ${folder}!`);
+    const folder = core.getInput("folder");
+    console.log(`folder ${folder}!`);
 
-  await upload.nonInteractive({
-    "bundle-dir": folder,
-    "organization-id": organizationId,
-    "definition-id": appDefinitionId,
-    token: accessToken,
-  });
+    await upload.nonInteractive({
+      "bundle-dir": folder,
+      "organization-id": organizationId,
+      "definition-id": appDefinitionId,
+      token: accessToken,
+    });
 
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 };
+
+deploy();
