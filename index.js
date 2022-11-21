ba91d2a53f0f9c1955064928ef29b87416f7bea8
vikaskumr <vikas.kumar@contenful.com>
2022-11-21 17:38:53 +0530

    fix: options passing to upload package

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ const deploy = async function () {
     console.log(`folder ${folder}!`);
 
     await upload.nonInteractive({
-      "bundle-dir": folder,
-      "organization-id": organizationId,
-      "definition-id": appDefinitionId,
-      token: accessToken,
+      "--bundle-dir": folder,
+      "--organization-id": organizationId,
+      "--definition-id": appDefinitionId,
+      "--token": accessToken,
     });
 
     const time = new Date().toTimeString();
