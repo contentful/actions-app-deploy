dd7d9d04e0057955f4436e5a59570805e6fc85df
vikaskumr <vikas.kumar@contenful.com>
2022-11-22 22:04:15 +0530

    chore: codebase updated to typescript

---
+++ build/index.js
@@ -0,0 +1,22 @@
+import { upload } from "@contentful/app-scripts";
+import core from "@actions/core";
+const deploy = async function () {
+    try {
+        const organizationId = core.getInput("organization-id");
+        const appDefinitionId = core.getInput("app-definition-id");
+        const accessToken = core.getInput("access-token");
+        const folder = core.getInput("folder");
+        await upload.nonInteractive({
+            bundleDir: folder,
+            organizationId,
+            definitionId: appDefinitionId,
+            token: accessToken,
+        });
+        const time = new Date().toTimeString();
+        core.setOutput("time", time);
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+};
+deploy();
---
+++ index.ts
@@ -0,0 +1,23 @@
+import { upload } from "@contentful/app-scripts";
+import core from "@actions/core";
+
+const deploy = async function (): Promise<void> {
+  try {
+    const organizationId: string = core.getInput("organization-id");
+    const appDefinitionId: string = core.getInput("app-definition-id");
+    const accessToken: string = core.getInput("access-token");
+    const folder: string = core.getInput("folder");
+
+    await upload.nonInteractive({
+      bundleDir: folder,
+      organizationId,
+      definitionId: appDefinitionId,
+      token: accessToken,
+    });
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+};
+deploy();
