d29ef67ea08f66235974dbdd142797b64114b3c5
vikaskumr <vikas.kumar@contenful.com>
2022-11-22 20:06:40 +0530

    fix: fixing build script

--- build/index.js
+++ build/index.js
@@ -1,27 +1,22 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_scripts_1 = require("@contentful/app-scripts");
-const core_1 = __importDefault(require("@actions/core"));
+import { upload } from "@contentful/app-scripts";
+import core from "@actions/core";
 const deploy = async function () {
     try {
-        const organizationId = core_1.default.getInput("organization-id");
-        const appDefinitionId = core_1.default.getInput("app-definition-id");
-        const accessToken = core_1.default.getInput("access-token");
-        const folder = core_1.default.getInput("folder");
-        await app_scripts_1.upload.nonInteractive({
+        const organizationId = core.getInput("organization-id");
+        const appDefinitionId = core.getInput("app-definition-id");
+        const accessToken = core.getInput("access-token");
+        const folder = core.getInput("folder");
+        await upload.nonInteractive({
             bundleDir: folder,
             organizationId,
             definitionId: appDefinitionId,
             token: accessToken,
         });
         const time = new Date().toTimeString();
-        core_1.default.setOutput("time", time);
+        core.setOutput("time", time);
     }
     catch (error) {
-        core_1.default.setFailed(error.message);
+        core.setFailed(error.message);
     }
 };
 deploy();
