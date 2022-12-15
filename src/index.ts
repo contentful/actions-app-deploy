dfe00c4df1e1865ee0c9680d87e023fc693e2b90
Vikas Kumar <vikas.kumar@contentful.com>
2022-12-15 22:36:37 +0530

    chore: github action in typescript [ EXT-4116 ] (#4)
    * chore: typescript code base
    
    * fix: execution file path in action
    
    * fix: build script
    
    * fix: build script
    
    * fix: removed console.logs
    
    * fix: build script
    
    * fix: build script tested
    
    * fix: type module added in package.json
    
    * fix: build
    
    * fix: testing github action using ts
    
    * fix: fixing build script
    
    * test: testing build script
    
    * test: testing build script
    
    * test: testing ncc bundler
    
    * fix: esbuild removed with ncc as it can not import dynamic modules
    
    * test: testing github action
    
    * test: tetsing action
    
    * test: testing
    
    * fix: testing
    
    * fix: deleted build.js file
    
    * fix: action.yml fixed
    
    * test: testing github action without dist folder
    
    * fix: dist folder added
    
    * feat: added husky for precommit hook
    
    * feat: modifying precommit hook to handle git add
    
    * test: testing precommit hook
    
    * feat: modifying precommit to add dist folder to avoid extra commit
    
    * chore: updated package.json, moved index.ts in src
    
    * fix: removing xdg-open file
    
    * fix: added command in husky to remove xdg-open file
    
    * fix: removed precommit hook for removing xdg-open file
    
    * chore: added name, license, author in  package.json, changed function format in index.ts
    
    * feat: tsconfig/bases used to make tsconfig consistent
    
    * feat: tsconfig/bases used to make tsconfig consistent
    
    * fix: license in package.json
    
    * feat: prettier
    
    * feat: introduce tsconfig/bases for consistency
    
    * chore: bumped contentful/app-scripts version to 1.5.1
    
    * feat: github action fully migrated to typescript
    
    Co-authored-by: vikaskumr <vikas.kumar@contenful.com>

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { upload } from "@contentful/app-scripts";
+import core from "@actions/core";
+
+async function deploy(): Promise<void> {
+  try {
+    const organizationId: string = core.getInput("organization-id");
+    const appDefinitionId: string = core.getInput("app-definition-id");
+    const accessToken: string = core.getInput("access-token");
+    const folder: string = core.getInput("folder");
+
+    await upload.nonInteractive({
+      bundleDir: folder,
+      organizationId,
+      definitionId: appDefinitionId,
+      token: accessToken,
+      userAgentApplication: "contentful.actions-app-deploy",
+    });
+  } catch (error) {
+    core.setFailed(`${(error as any)?.message ?? error}`)
+  }
+}
+
+deploy();
